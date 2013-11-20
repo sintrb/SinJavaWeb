f46138019a66701721468f80b5e58c4370cb07ba
RobinTang <sintrb@gmail.com>
2013-11-20 14:05:08 +0800

    HTTP status code 304

---
+++ src/com/sin/java/web/server/exception/WebException304NotModified.java
@@ -0,0 +1,21 @@
+package com.sin.java.web.server.exception;
+
+import com.sin.java.web.server.BaseWebException;
+
+/**
+ * HTTP 304 Nof Modified <br/>
+ * 
+ * @author trb
+ * @date 2013-11-20
+ */
+public class WebException304NotModified extends BaseWebException {
+	private static final long serialVersionUID = 5347862129979078112L;
+
+	public static final int CODE = 304;
+	public static final String DESCRIBE = "Not Modified";
+	public static final String RESPONSE = null;// "<center><strong>304 Not Modified.</strong></center>";
+
+	public WebException304NotModified() {
+		super(CODE, DESCRIBE, RESPONSE);
+	}
+}
