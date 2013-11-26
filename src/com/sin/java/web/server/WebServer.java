c303bffc1df8995be1270604edd3dfd36883318d
RobinTang <sintrb@gmail.com>
2013-11-26 19:12:40 +0800

    version 1.0 finished

--- src/com/sin/java/web/server/WebServer.java
+++ src/com/sin/java/web/server/WebServer.java
@@ -77,6 +77,12 @@ public class WebServer {
 		this.logger = logger;
 	}
 
+	/**
+	 * Start server
+	 * 
+	 * @return return true when start success, otherwise return false(maybe open
+	 *         socket fail)
+	 */
 	public boolean start() {
 		try {
 			this.serverSocket = new ServerSocket(port);
@@ -91,6 +97,11 @@ public class WebServer {
 		return false;
 	}
 
+	/**
+	 * Stop server and close server socket
+	 * 
+	 * @return always return true
+	 */
 	public boolean stop() {
 		this.setStatus(Status.Stoped);
 		try {
@@ -102,60 +113,136 @@ public class WebServer {
 		return true;
 	}
 
+	/**
+	 * Suspend server
+	 * 
+	 * @return always return true
+	 */
 	public boolean pause() {
 		this.setStatus(Status.Paused);
 		return true;
 	}
 
+	/**
+	 * Resume server
+	 * 
+	 * @return always return true
+	 */
 	public boolean resume() {
 		this.setStatus(Status.Running);
 		return true;
 	}
 
+	/**
+	 * Get the server main socket, in fact it is unnecessary to call this method
+	 * 
+	 * @return the server socket
+	 */
 	public ServerSocket getServerSocket() {
 		return serverSocket;
 	}
 
+	/**
+	 * Get the server status
+	 * 
+	 * @return server status
+	 */
 	public Status getStatus() {
 		return status;
 	}
 
+	/**
+	 * Set the server status
+	 */
 	public void setStatus(Status status) {
 		this.status = status;
 		log("Server %s", status.toString());
 	}
-	
-	public void setThreadpool(Executor threadPool){
+
+	/**
+	 * Set thread pool to handle client request
+	 * 
+	 * @param threadPool
+	 *            Thread pool, if pool is null the server will create a new
+	 *            thread to handle each client request
+	 */
+	public void setThreadpool(Executor threadPool) {
 		this.threadPool = threadPool;
 	}
-	
+
+	/**
+	 * Get thread pool, it will called by ServerThread
+	 * 
+	 * @return ThreadPool
+	 */
 	public Executor getThreadPool() {
 		return threadPool;
 	}
 
+	/**
+	 * Add a request handler
+	 * 
+	 * @param urlregex
+	 *            request path pattern
+	 * @param handler
+	 *            request handler, contain class and method, f.e.
+	 *            "com.sin.web.ApiHanlder.getuser"
+	 */
 	public void addHandler(String urlregex, String handler) {
 		this.addHandler(urlregex, handler, null, "GET");
 	}
 
-	public void addHandler(String urlregex, String handler,
-			Map<String, Object> injectMap) {
+	/**
+	 * Add a request handler
+	 * 
+	 * @param urlregex
+	 *            request path pattern
+	 * @param handler
+	 *            request handler, contain class and method, f.e.
+	 *            "com.sin.web.ApiHanlder.getuser"
+	 * @param injectMap
+	 *            inject variable mapping
+	 */
+	public void addHandler(String urlregex, String handler, Map<String, Object> injectMap) {
 		this.addHandler(urlregex, handler, injectMap, "GET");
 	}
 
+	/**
+	 * Add a request handler
+	 * 
+	 * @param urlregex
+	 *            request path pattern
+	 * @param handler
+	 *            request handler, contain class and method, f.e.
+	 *            "com.sin.web.ApiHanlder.getuser"
+	 * @param injectMap
+	 *            inject variable mapping
+	 * @param method
+	 *            HTTP method, f.e: GET POST PUT etc, default is GET
+	 */
 	public void addHandler(String urlregex, String handler, String method) {
 		this.addHandler(urlregex, handler, null, method);
 	}
 
-	public void addHandler(String urlregex, String handler,
-			Map<String, Object> injectMap, String method) {
+	/**
+	 * Add a request handler
+	 * 
+	 * @param urlregex
+	 *            request path pattern
+	 * @param handler
+	 *            request handler, contain class and method, f.e.
+	 *            "com.sin.web.ApiHanlder.getuser"
+	 * @param method
+	 *            HTTP method, f.e: GET POST PUT etc, default is GET
+	 */
+	public void addHandler(String urlregex, String handler, Map<String, Object> injectMap, String method) {
 		int ix = handler.lastIndexOf('.');
 		String handlerClassName = handler.substring(0, ix);
 		String handlerMethodName = handler.substring(ix + 1);
 		try {
 			Class<?> handlerClass = Class.forName(handlerClassName);
 			if (BaseHandler.class.isAssignableFrom(handlerClass) == false) {
-				throw new Exception(String.format("%s is not extends from %s.",
-						handlerClassName, BaseHandler.class.getName()));
+				throw new Exception(String.format("%s is not extends from %s.", handlerClassName, BaseHandler.class.getName()));
 			}
 			Method[] handlerMethods = handlerClass.getMethods();
 			Method handlerMethod = null;
@@ -165,57 +252,114 @@ public class WebServer {
 				}
 			}
 			if (handlerMethod == null) {
-				throw new Exception(String.format(
-						"%s has not method is named %s.", handlerClassName,
-						handlerMethodName));
+				throw new Exception(String.format("%s has not method is named %s.", handlerClassName, handlerMethodName));
 			}
 			if (String.class.isAssignableFrom(handlerMethod.getReturnType()) == false) {
-				throw new Exception(String.format(
-						"%s return type is not String.", handler));
+				throw new Exception(String.format("%s return type is not String.", handler));
 			}
-			UrlregexMappingItem urlHandlerItem = new UrlregexMappingItem(
-					urlregex, method, handler, handlerClassName,
-					handlerMethodName, injectMap);
+			UrlregexMappingItem urlHandlerItem = new UrlregexMappingItem(urlregex, method, handler, handlerClassName, handlerMethodName, injectMap);
 			this.urlsMapping.add(urlHandlerItem);
 		} catch (Exception e) {
 			this.err(e);
 		}
 	}
 
+	/**
+	 * Find and get a UrlregexMappingItem by request path and method
+	 * 
+	 * @param url
+	 *            request path
+	 * @param method
+	 *            HTTP method
+	 * @return
+	 */
 	public UrlregexMappingItem getHandler(String url, String method) {
 		return this.urlsMapping.get(url, method);
 	}
+	
+	
+	
 
 	// for Logger
+	/**
+	 * Log a string
+	 * 
+	 * @param l
+	 *            log string
+	 */
 	public void log(String l) {
 		if (this.logger != null)
 			this.logger.log(l);
 	}
 
+	/**
+	 * Log by format
+	 * 
+	 * @param format
+	 *            format string
+	 * @param args
+	 *            arguments
+	 */
 	public void log(String format, Object... args) {
 		this.log(String.format(format, args));
 	}
 
+	/**
+	 * Log a information string
+	 * 
+	 * @param i
+	 *            information string
+	 */
 	public void inf(String i) {
 		if (this.logger != null)
 			this.logger.inf(i);
 	}
 
+	/**
+	 * Log information by format
+	 * 
+	 * @param format
+	 *            format string
+	 * @param args
+	 *            arguments
+	 */
 	public void inf(String format, Object... args) {
 		this.inf(String.format(format, args));
 	}
 
+	/**
+	 * Log a error string
+	 * 
+	 * @param e
+	 *            error string
+	 */
 	public void err(String e) {
 		if (this.logger != null)
 			this.logger.err(e);
 	}
 
+	/**
+	 * Log a Exception
+	 * 
+	 * @param e
+	 *            Exception
+	 */
 	public void err(Exception e) {
 		if (this.logger != null)
 			this.logger.err(e);
 	}
 
+	/**
+	 * Log error by format
+	 * 
+	 * @param format
+	 *            format string
+	 * @param args
+	 *            arguments
+	 */
 	public void err(String format, Object... args) {
 		this.err(String.format(format, args));
 	}
 }
+
+
